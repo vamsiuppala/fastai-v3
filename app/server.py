3fc5ff80978fbc3883f10f919d531e59f8307a87
Vamsi Uppala <uvvamsikrishna@gmail.com>
2019-02-06 19:24:34 -0500

    change classes to reflect shoes and change model url to reflect shoes model on google drive

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1In5PD8BTioT4r1Mpjz3EMu9urKZgzvJT'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['chelseaboot', 'chukkaboot', 'derbyboot', 'loafers', 'monkstrap', 'operapump', 'oxfords']
 path = Path(__file__).parent
 
 app = Starlette()
